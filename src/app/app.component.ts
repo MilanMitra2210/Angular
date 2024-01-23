0028e3ca2ea2b9846b74c8bf4b1c7463be72c61f
MilanMitra2210 <milanmitranda2020@gmail.com>
2024-01-23 16:01:34 +0530

    Bootstrap and Components Adding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegistrationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Milan Mitra';
 }
